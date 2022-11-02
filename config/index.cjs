2c294b96722cbb77e910722208b3f88dda3fc20c
Gengarrr <49379977+Gengarrr@users.noreply.github.com>
2022-11-02 20:22:33 +0800

    1102-4

--- config/index.cjs
+++ config/index.cjs
@@ -15,8 +15,14 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '',
+  PROVINCE: '江苏',
   CITY: '南京',
+  
+  SWITCH: {
+    weather: true,
+    earthyLoveWords: true,
+  }
+  
 
   USERS: [
     {
@@ -48,34 +54,33 @@ const USER_CONFIG = {
       ],
     },
     
+    
    {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '圆小方',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU17504Tq7cVi0YeRQ45yo6V2U6n7eFNsHLp6PyP',
+      id: 'PDU17503TUlAABpjigg8NRYo6dKUZHrOKuJaiv37y',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '0002',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-10',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '圆小方', year: '1999', date: '10-10',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '圆小方', year: '1999', date: '10-10',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '表白纪念日', year: '2022', date: '8-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-08-22' },
       ],
     }, 
    
