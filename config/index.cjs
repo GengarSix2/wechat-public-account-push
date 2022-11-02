79d98f6983ba0db3a6aba35cd82e27995ccfd6c9
Gengarrr <49379977+Gengarrr@users.noreply.github.com>
2022-11-02 21:03:52 +0800

    add baby

--- config/index.cjs
+++ config/index.cjs
@@ -60,7 +60,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU17504Tq7cVi0YeRQ45yo6V2U6n7eFNsHLp6PyP',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0002',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-10',
       festivals: [
@@ -87,7 +87,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
