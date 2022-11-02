83ee472a412fc6e72d9a76fb61d5b473cecc688c
Gengarrr <49379977+Gengarrr@users.noreply.github.com>
2022-11-02 21:20:10 +0800

    1102-10

--- config/index.cjs
+++ config/index.cjs
@@ -34,10 +34,6 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-02',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: 'GC', year: '2000', date: '06-02',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: 'GC', year: '2000', date: '06-02',
@@ -64,10 +60,6 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-10',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '圆小方', year: '1999', date: '10-10',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '圆小方', year: '1999', date: '10-10',
